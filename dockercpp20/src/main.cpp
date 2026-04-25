b213d1db0df0aa232ebf039e3f98abc4de21aadf
DESKTOP-9FHAT6E\cdk <dk990617@outlook.com>
2026-04-25 23:30:54 +0800

    2026-04-25 更新

---
+++ dockercpp20/src/main.cpp
@@ -0,0 +1,12 @@
+import<iostream>;
+import<vector>;
+import<format>;
+//#include <gtest/gtest.h>
+//#include "hw1.h"
+
+int main(int argc, char **argv)
+{
+    using std::format;
+    std::cout << "Running main() from main.cpp\n";
+    return 0;
+}
